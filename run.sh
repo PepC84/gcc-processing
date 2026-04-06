e91e637a1b20e9489b3fbf50eccb7121185bd44f
Jose Llamas <pep84c@gmail.com>
2026-04-05 21:20:28 -0700

    update3

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# Build a sketch and immediately run it
-# Usage: ./run.sh [path/to/sketch.cpp] [output_name]
-set -e
-bash build.sh "${1:-src/MySketch.cpp}" "${2:-SketchApp}"
-./"${2:-SketchApp}"
